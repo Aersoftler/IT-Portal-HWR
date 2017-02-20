d9a1e7f2a5688e7cfe9876f88bf4139767a57244
Aersoftler <davidgolla@web.de>
2017-02-20 12:34:20 +0100

    Startseite hinzugefügt

---
+++ js/home.js
@@ -0,0 +1,22 @@
+/**
+ * Created by dgolla on 19.02.2017.
+ */
+
+//Wird aufgerufen, wenn die Seite fertig geladen hat
+window.onload = function () {
+    load();
+};
+
+//Wird aufgerufen, wenn sich die Auflösung der Seite verändert hat
+window.onresize = function () {
+    load();
+};
+
+//Wird genutzt, um die Seite korrekt anzuzeigen
+function load() {
+    var width = window.innerWidth;
+    for (i = 0; i < 4; i++) {
+        var itemId = "item" + i;
+        document.getElementById(itemId).style.height = (width / 5) < 200 ? "200px" : (width / 5) + "px";
+    }
+}
