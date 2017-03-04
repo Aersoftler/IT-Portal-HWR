209ffdacd8683321acc86dabfb579535e716c939
Aersoftler <davidgolla@web.de>
2017-03-04 20:45:27 +0100

    Pfade der FooterLinks korregiert

--- client/js/addHeaderFooter.js
+++ client/js/addHeaderFooter.js
@@ -1,5 +1,5 @@
 function showFooter() {
-    document.getElementById("foo").innerHTML = "<div><div class=\"container\"><hr></div><p class=\"col-sm-offset-2 col-sm-8\" style=\"text-align: center\"><a href=\"ueber_uns\">Über uns</a><a href=\"kontakt\" style=\"padding-right: 15vw; padding-left: 15vw\">Kontakt</a><a href=\"impressum\">Impressum</a></p></div>";
+    document.getElementById("foo").innerHTML = "<div><div class=\"container\"><hr></div><p class=\"col-sm-offset-2 col-sm-8\" style=\"text-align: center\"><a href=\"\\ueber_uns\">Über uns</a><a href=\"\\kontakt\" style=\"padding-right: 15vw; padding-left: 15vw\">Kontakt</a><a href=\"\\impressum\">Impressum</a></p></div>";
 }
 
 function showHeader() {
