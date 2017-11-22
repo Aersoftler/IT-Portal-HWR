08458f6b15827218e786fe47634ad68fb66e16eb
LeonieEbert <ebert.leonie@gmail.com>
2017-11-22 16:28:02 +0100

    Elemente in Selectionliste sind unique und sortiert

--- client/js/uebersicht.js
+++ client/js/uebersicht.js
@@ -34,6 +34,8 @@ module.controller(
         $http.get('/website').then(function (response) {
             fillScopeResults(response);
         });*/
+
+    results
         $scope.results = results;
     }
 ]);
@@ -44,4 +46,43 @@ module.directive('dieSuchergebnisse', function () {
         templateUrl: '/suchergebnisse.html',
         restrict: 'A'
     }
+});
+
+module.filter('unique', function () {
+
+    return function (items, filterOn) {
+
+        if (filterOn === false) {
+            return items;
+        }
+
+        if ((filterOn || angular.isUndefined(filterOn)) && angular.isArray(items)) {
+            var hashCheck = {}, newItems = [];
+
+            var extractValueToCompare = function (item) {
+                if (angular.isObject(item) && angular.isString(filterOn)) {
+                    return item[filterOn];
+                } else {
+                    return item;
+                }
+            };
+
+            angular.forEach(items, function (item) {
+                var valueToCheck, isDuplicate = false;
+
+                for (var i = 0; i < newItems.length; i++) {
+                    if (angular.equals(extractValueToCompare(newItems[i]), extractValueToCompare(item))) {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+                if (!isDuplicate) {
+                    newItems.push(item);
+                }
+
+            });
+            items = newItems;
+        }
+        return items;
+    };
 });
